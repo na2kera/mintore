a299e68c0e9f50aa39d265f7368ed80307cadfb9
sora-00 <sora.umiushi@gmail.com>
2024-09-02 13:26:17 +0900

    ラップ変換,paddingをmarginに変更

--- app/components/body.tsx
+++ app/components/body.tsx
@@ -4,94 +4,57 @@ import Image from "next/image";
 const Body = () => {
   return (
     <>
-      <Box display={"flex"} alignItems={"center"} flexDirection={"column"}>
-        <Box position={"relative"}>
-          <Image
-            src="/kinnikun.jpg"
-            alt="bodyimage"
-            width={1200}
-            height={880}
-          />
-        </Box>
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        position="relative"
+      >
+        <Image src="/kinnikun.jpg" alt="bodyimage" width={1200} height={880} />
         {/* <Box
           position={"absolute"}
           bgcolor={"#000000"}
           width={"80px"}
           height={"10px"}
-          marginTop={11.5}
+          marginBottom={86}
           marginLeft={7.5}
         ></Box> */}
+        <Box position={"absolute"} marginRight={70} marginBottom={60}>
+          <Link href="/ude" color="#000000" fontSize={"60px"}>
+            腕
+          </Link>
+        </Box>
+        <Box position={"absolute"} marginRight={100} marginBottom={35}>
+          <Link href="/haikin" color="#000000" fontSize={"60px"}>
+            背筋
+          </Link>
+        </Box>
+        <Box position={"absolute"} marginRight={98} marginTop={20}>
+          <Link href="/ashi" color="#000000" fontSize={"60px"}>
+            脚
+          </Link>
+        </Box>
+        <Box position={"absolute"} marginLeft={80} marginBottom={90}>
+          <Link href="/kata" color="#000000" fontSize={"60px"}>
+            肩
+          </Link>
+        </Box>
 
-        <Link
-          href="/ude"
-          position={"absolute"}
-          paddingRight={70}
-          paddingTop={20}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          腕
-        </Link>
-        <Link
-          href="/haikin"
-          position={"absolute"}
-          paddingRight={98}
-          paddingTop={34}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          背筋
-        </Link>
-        <Link
-          href="/ashi"
-          position={"absolute"}
-          paddingRight={98}
-          paddingTop={60}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          脚
-        </Link>
-        <Link
-          href="/kata"
-          position={"absolute"}
-          paddingLeft={78}
-          paddingTop={8}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          肩
-        </Link>
-        <Link
-          href="/kyokin"
-          position={"absolute"}
-          paddingLeft={96}
-          paddingTop={25}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          胸筋
-        </Link>
-        <Link
-          href="/hukkin"
-          position={"absolute"}
-          paddingLeft={105}
-          paddingTop={48}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          腹筋
-        </Link>
-        <Link
-          href="/zenshin"
-          position={"absolute"}
-          paddingLeft={120}
-          paddingTop={80}
-          color="#000000"
-          fontSize={"60px"}
-        >
-          全身
-        </Link>
+        <Box position={"absolute"} marginLeft={100} marginBottom={55}>
+          <Link href="/kyokin" color="#000000" fontSize={"60px"}>
+            胸筋
+          </Link>
+        </Box>
+        <Box position={"absolute"} marginLeft={108} marginBottom={5}>
+          <Link href="/fukkin" color="#000000" fontSize={"60px"}>
+            腹筋
+          </Link>
+        </Box>
+        <Box position={"absolute"} marginLeft={100} marginTop={60}>
+          <Link href="/zenshin" color="#000000" fontSize={"80px"}>
+            全身
+          </Link>
+        </Box>
       </Box>
     </>
   );
