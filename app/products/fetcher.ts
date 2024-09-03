702db93533744facd586df193e09c5988b3f87d4
na2kera <nat3mitaro@gmail.com>
2024-09-03 22:33:21 +0900

    コメントを追加

--- app/products/fetcher.ts
+++ app/products/fetcher.ts
@@ -1,8 +1,10 @@
+//データベースから読み取る用の関数を書いておく
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
 const supabase = createClient();
 
+//ユーザーがログインしているかを判定する関数
 export async function isAuthenticated() {
   const {
     data: { user },
