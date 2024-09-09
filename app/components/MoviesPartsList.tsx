4fc1882e20b135c5541b8fe40cdee5ee0a4682e4
na2kera <nat3mitaro@gmail.com>
2024-09-10 03:32:59 +0900

    ブックマークのUIを実装

--- app/components/MoviesPartsList.tsx
+++ app/components/MoviesPartsList.tsx
@@ -6,11 +6,18 @@ import { Video } from "../types/video";
 import Image from "next/image";
 import { Box, Button, Typography } from "@mui/material";
 import Link from "next/link";
+import { User } from "../types/user";
+import MoviePartsList from "./MoviePartsList";
 
-const supabase = createClient();
+type Props = {
+  userData: User;
+};
 
-const MoviesPartsList = () => {
+const MoviesPartsList = ({ userData }: Props) => {
+  console.log(userData);
   const pathname = usePathname();
+
+  const supabase = createClient();
 
   const [videos, setVideos] = useState<Video[]>([]);
 
@@ -32,45 +39,7 @@ const MoviesPartsList = () => {
   return (
     <>
       {videos?.map((video) => {
-        return (
-          <Box
-            key={video.id}
-            display={"flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-            pt={5}
-          >
-            <Box
-              display={"flex"}
-              alignItems={"center"}
-              gap={3}
-              bgcolor={"#F8EFA0"}
-              width={"70%"}
-              height={"25vh"}
-              pl={30}
-              position={"relative"}
-            >
-
-              <Link href={video.youtube_url}>
-                <Image
-                  src={video.thumbnail}
-                  width={350}
-                  height={350}
-                  alt="thumbnail"
-                />
-              </Link>
-              <Box>
-                <Typography fontSize={25}>{video.title}</Typography>
-                <Link href={`/post/${video.youtube_url.split("/")[3]}`}>
-                  <Button variant="contained" color="primary">
-                    投稿
-                  </Button>
-                </Link>
-              </Box>
-
-            </Box>
-          </Box>
-        );
+        return <MoviePartsList video={video} userData={userData} />;
       })}
     </>
   );
